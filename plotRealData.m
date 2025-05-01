c2b38ed960f555e97f818bcbbd492f3623d66510
shumengc <shumengchen1@gmail.com>
2025-04-30 23:32:55 -0400

    Delete plotRealData.m

--- plotRealData.m
+++
@@ -1,9 +0,0 @@
-load('/Users/chenshumeng/Desktop/code/MATLABFunction/MATLABCode_Chen/EmGm/results.mat', 'results');
-load('/Users/chenshumeng/Desktop/code/MATLABFunction/MATLABCode_Chen/EmGm/results_sv.mat', 'results_sv');
-
-fieldName='K151'
-UpdateperSeq=2
-
-RealDataFRT(results, results_sv,fieldName,UpdateperSeq)
-
-
